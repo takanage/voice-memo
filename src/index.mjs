29dc62b22bca360a3f76e47a835b655f47c77c33
Willian Takanage <williantakanage@gmail.com>
2021-05-09 10:27:32 -0300

    Created Project

---
+++ src/index.mjs
@@ -0,0 +1,10 @@
+
+import Controller from "./controller.mjs";
+import View from "./view.mjs";
+
+const view = new View()
+
+
+Controller.initialize({
+    view
+})
---
+++ src/view.mjs
@@ -0,0 +1,19 @@
+export default class View {
+    constructor () {
+        this.btnStart = document.getElementById('btnStart');
+        this.btnStop = document.getElementById('btnStop');
+        this.audioElement = document.getElementById('audio');
+    };
+
+    onRecordClick(command) {
+
+        return () => {
+            command()
+        }
+    }
+
+    configureStartRecordingButton(command) {
+        this.btnStart.addEventListener('click',this.onRecordClick(command) )
+    }
+
+};
